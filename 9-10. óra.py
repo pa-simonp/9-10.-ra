e9955e48ca818b15744544ad0d5e2ab775f6673d
pa-simonp <112852487+pa-simonp@users.noreply.github.com>
2022-10-03 09:35:07 +0200

    Add files via upload

---
+++ 9-10. óra.py
@@ -0,0 +1,10 @@
+#első feladat 9-10
+idei_év=2022
+print(type(idei_év))
+print("Az idei év változó éréke: ", idei_év , sep="-->")
+felhasználó_kora=input("hány éves vagy?")
+print("te most", felhasználó_kora, "éves vagy.")
+print("felhasználó_kora változó ",type(felhasználo_kora))
+felhasználó_kora = int(felhasználó_kora)
+születési_év = idei_év - felhasználó_kora
+print("ekkor születtél:" , születési_év, ".", sep="")
